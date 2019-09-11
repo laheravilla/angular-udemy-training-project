827902317818aa68a740b04fa151f6bdb1e31a38
laheravilla <laheravilla@gmail.com>
2019-09-11 05:10:39 +0200

    HttpClient service created

---
+++ src/app/services/project.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from '../models/project';
+import { Global } from './global';
+
+@Injectable()
+export class ProjectService
+{
+    public url: string;
+
+    constructor(
+        private _http: HttpClient
+    ) {
+        this.url = Global.url;
+    }
+
+    testService()
+    {
+        return 'Testing Angular service';
+    }
+}
